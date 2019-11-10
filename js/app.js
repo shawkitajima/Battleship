3d435cbd3ebed936e27f27069ad09ba0ffafef66
Shaw Kitajima <shawkitajima@gmail.com>
2019-11-10 10:04:25 -0800

    adds computer logic and modifies other flaws

--- js/app.js
+++ js/app.js
@@ -49,6 +49,7 @@ let computerShipChecker;
 let computerHits, computerMisses, userHits, userMisses;
 let internalCounter;
 let stopper;
+let hitCheck, hitIndex, moveUp, moveLeft, moveRight, moveDown, checkVertical;
 
 /*----- app's state (variables) -----*/
 document.querySelector('button').addEventListener('click', function() {
@@ -78,6 +79,13 @@ function init() {
     computerHits = [];
     userHits = [];
     userMisses = [];
+    hitCheck = false;
+    moveRight = 1;
+    moveLeft = 0;
+    moveRight = 0;
+    moveDown = 0;
+    moveUp = 0;
+    checkVertical = true;
     message.textContent = `Please place your ${USERSHIPS[userShipCounter].type} - ${USERSHIPS[userShipCounter].spotLength} pieces`;
     grid.removeEventListener('click', receivePlayerGuess);
     grid.addEventListener('click', populateUserShips);
@@ -170,6 +178,13 @@ function checkUserGuess(guess) {
 function checkComputerGuess(guess) {
     if (allUserSpots.includes(parseInt(guess))) {
         computerHits.push(guess);
+        hitCheck = true;
+        hitIndex = guess;
+        checkVertical = true;
+        moveRight = 1;
+        moveLeft = -1;
+        moveUp = -10;
+        moveDown = 10;
         playKerboom();
     }
     else {
@@ -178,6 +193,18 @@ function checkComputerGuess(guess) {
     }
 }
 
+function checkComputerNonRandomGuess(guess) {
+    if (allUserSpots.includes(parseInt(guess))) {
+        computerHits.push(guess);
+        playKerboom();
+        return true;
+    }
+    else {
+        computerMisses.push(guess);
+        playSploosh();
+        return false;
+    }
+}
 
 function playerTurn() {
     message.textContent = `Player's Turn`;
@@ -186,26 +213,32 @@ function playerTurn() {
 }
 
 function receivePlayerGuess(evt) {
-    grid.removeEventListener('click', receivePlayerGuess);
     let guess = evt.target.id;
-    checkUserGuess(guess);
-    renderBoard(userHits, userMisses);  
-    if (stopper) return;
-    if (checkWinner(userHits, allComputerSpots)) {
-        message.textContent = 'You Win!';
-        setTimeout(function() {
-            playWinning();
-        }, 1500)
-        return;
-    }   
-    setTimeout(receiveComputerGuess, 2000);
+    if (!(userHits.includes(parseInt(guess))) && !(userMisses.includes(parseInt(guess)))) {
+        grid.removeEventListener('click', receivePlayerGuess);
+        checkUserGuess(guess);
+        renderBoard(userHits, userMisses);  
+        if (stopper) return;
+        if (checkWinner(userHits, allComputerSpots)) {
+            message.textContent = 'You Win!';
+            setTimeout(function() {
+                playWinning();
+            }, 1500)
+            return;
+        }   
+        setTimeout(receiveComputerGuess, 2000);
+    }
 }
 
 function receiveComputerGuess() {
     if (stopper) return;
     message.textContent = `Computer's Turn!`;
+    if (hitCheck) {
+        getLinkedGuess();
+        return;
+    }
     computerGuess = Math.floor(Math.random() * Math.floor(100));
-    while (computerHits.includes(computerGuess) && computerMisses.includes(computerGuess)) {
+    while (computerHits.includes(computerGuess) || computerMisses.includes(computerGuess)) {
         computerGuess = Math.floor(Math.random() * Math.floor(100));
     }
     checkComputerGuess(computerGuess);
@@ -251,3 +284,92 @@ function playLosing() {
     LOSING.type = 'audio/ogg';
     LOSING.play();
 }
+
+
+// Variables:
+// hitCheck (Boolean) becomes true if random guess yields a hit
+
+// hitIndex: the index that got a hit, which triggers the getLinkedGuess function
+
+// moveUp: moves guess -10(i) indices
+
+// moveLeft: moves guess -1(i) indices
+
+// moveRight: moves guess 1(i) indices
+
+// moveDown: moves guess 10(i) indices
+
+// BetterGuesses: array to contain linked guesses
+
+// Functions:
+
+
+function getLinkedGuess() {
+    if (moveRight && (hitIndex + moveRight) % 10 < 10 && !(computerHits.includes(hitIndex + moveRight)) && !(computerMisses.includes(hitIndex + moveRight))) {
+        if (checkComputerNonRandomGuess(hitIndex + moveRight)) {
+            moveRight++;
+            checkVertical = false;
+        }
+        else {
+            moveRight = 0;
+        } 
+    }
+    else if (moveLeft && ((hitIndex + moveLeft) % 10 >= 0) && !(computerHits.includes(hitIndex + moveLeft)) && !(computerMisses.includes(hitIndex + moveLeft))) {
+        if (checkComputerNonRandomGuess(hitIndex + moveLeft)) {
+            moveLeft = moveLeft - 1;
+            checkVertical = false;
+        }
+        else {
+            moveLeft = 0;
+        }
+    }
+    else if (moveUp && checkVertical && (hitIndex + moveUp > 0) && !(computerHits.includes(hitIndex + moveUp)) && !(computerMisses.includes(hitIndex + moveUp))) {
+        if (checkComputerNonRandomGuess(hitIndex + moveUp)) {
+            moveUp = moveUp - 10;
+        }
+        else {
+            moveUp = 0;
+        }
+    }   
+    else if (moveDown && ((hitIndex + moveDown) < 100) && !(computerHits.includes(hitIndex + moveDown)) && !(computerMisses.includes(hitIndex + moveDown))) {
+        if (checkComputerNonRandomGuess(hitIndex + moveDown)) {
+            moveDown = moveDown + 10;
+        }
+        else {
+            moveDown = 0;
+            hitCheck = false;
+        }
+    } 
+    else {
+        hitCheck = false;
+        receiveComputerGuess();
+        return;
+    }  
+    if (checkWinner(computerHits, allUserSpots)) {
+        message.textContent = 'Computer Wins';
+        setTimeout(function() {
+            playLosing();
+        }, 1500)
+        return;
+    }
+    renderBoard(computerHits, computerMisses, allUserSpots);
+    setTimeout(playerTurn, 2000);
+}
+// function getLinkedGuess()
+    // this function gets called by the general guess function if hitCheck is true, it will then set CheckIndex to the hit index
+    // if this function is called, it will call the playerGuess function and return from the computerGuess function so that the random guess is not called 
+    // This function has a series of if/then statements that look like:
+    // if (BetterGuesses.length >= 5)
+        // hitCheck = false;
+        // call playerGuess
+        // return;
+    // if (moveRight && guess+moveRight % 10 < 9 && not guessed already)
+    // else if (moveUp && guess + moveUp % 10 > 1 && not guessed already))
+    // And so on
+    // Here is what the moveRight will if/then will do
+        //  guess the index: guess + moveRight
+        //  Update computer guess arrays, and add to BetterGuesses
+        // if (userArray.includes(guess + moveRight) 
+            // add 1 to moveRight
+        //  else 
+            // moveRight = 0;
